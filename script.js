3fa7f86742187b5119dd1bbd1ce351bdb9a81ad7
Nahush Tawlare <ntawlare@gmail.com>
2024-06-12 15:25:36 +0530

    firxt commit

---
+++ script.js
@@ -0,0 +1,110 @@
+const menubar = document.querySelector('#menu');
+const Navbar = document.querySelector('.navbar');
+menubar.onclick=()=>{
+    menubar.classList.toggle('bx-x');
+    Navbar.classList.toggle('active')
+}
+const section=document.querySelectorAll('section');
+const navlink = document.querySelectorAll('header nav a')
+window.onscroll = ()=>{
+    section.forEach(sec=>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id')
+        if(top>offset && top < offset + height){
+            sec.classList.add('start-animation');
+            navlink.forEach(links=>{
+                links.classList.remove('active')
+                document.querySelector('header nav a[href*='+id+']').classList.add('active')
+              
+            })
+        }
+    })
+    var header = document.querySelector('.header');
+    header.classList.toggle('sticky',window.scrollY>100)
+    menubar.classList.remove('bx-x');
+    Navbar.classList.remove('active')
+}
+
+
+// var words = document.getElementsByClassName('word');
+// var wordArray = [];
+// var currentWord = 0;
+
+// words[currentWord].style.opacity = 1;
+// for (var i = 0; i < words.length; i++) {
+//   splitLetters(words[i]);
+// }
+
+// function changeWord() {
+//   var cw = wordArray[currentWord];
+//   var nw = currentWord == words.length-1 ? wordArray[0] : wordArray[currentWord+1];
+//   for (var i = 0; i < cw.length; i++) {
+//     animateLetterOut(cw, i);
+//   }
+  
+//   for (var i = 0; i < nw.length; i++) {
+//     nw[i].className = 'letter behind';
+//     nw[0].parentElement.style.opacity = 1;
+//     animateLetterIn(nw, i);
+//   }
+  
+//   currentWord = (currentWord == wordArray.length-1) ? 0 : currentWord+1;
+// }
+
+// function animateLetterOut(cw, i) {
+//   setTimeout(function() {
+// 		cw[i].className = 'letter out';
+//   }, i*80);
+// }
+
+// function animateLetterIn(nw, i) {
+//   setTimeout(function() {
+// 		nw[i].className = 'letter in';
+//   }, 340+(i*80));
+// }
+
+// function splitLetters(word) {
+//   var content = word.innerHTML;
+//   word.innerHTML = '';
+//   var letters = [];
+//   for (var i = 0; i < content.length; i++) {
+//     var letter = document.createElement('span');
+//     letter.className = 'letter';
+//     letter.innerHTML = content.charAt(i);
+//     word.appendChild(letter);
+//     letters.push(letter);
+//   }
+  
+//   wordArray.push(letters);
+// }
+
+// changeWord();
+// setInterval(changeWord, 4000);
+
+
+document.getElementById('downloadBtn').addEventListener('click', function() {
+    // Create a new anchor element
+    var a = document.createElement('a');
+    // Set the href attribute to the path of the resume file
+    a.href = 'Full Responsive Portfolio'; // Make sure this path is correct
+    // Set the download attribute with a filename
+    a.download = 'NahushSDE1.pdf';
+    // Append the anchor to the body (necessary for the click to work in some browsers)
+    document.body.appendChild(a);
+    // Programmatically click the anchor to trigger the download
+    a.click();
+    // Remove the anchor from the document
+    document.body.removeChild(a);
+});
+
+document.getElementById('contactBtn').addEventListener('click', function() {
+    // WhatsApp contact number in international format (without + sign)
+    var contactNumber = '917972465878'; // Replace with the actual contact number
+    // WhatsApp URL schema
+    var whatsappUrl = 'https://wa.me/' + contactNumber;
+    // Open the URL in a new tab
+    window.open(whatsappUrl, '_blank');
+});
+
